55d9f1ff90777f66168293144e6787c7eacfff84
sawool <sawool82@gmail.com>
2020-05-05 21:38:54 +0900

    Header 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './containers/Home';
 import SignUp from './containers/SignUp';
 import SignIn from './containers/SignIn';
-
+import Header from './components/Header';
 import { Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
       <Route path="/home" component={Home} />
       <Route path="/signup" component={SignUp} />
       <Route path="/signin" component={SignIn} />
+      <Route path="/header" component={Header} />
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
+import './Header.css';
 
-function Header() {
-  return <Navbar bg="dark">Side Project with TypeScript</Navbar>;
+type HeaderProps = RouteComponentProps<any> & {
+  isSignin: boolean;
+  username: string;
+};
+
+function Header({ isSignin, username, history }: HeaderProps) {
+  return (
+    <>
+      <Navbar bg="primary" variant="dark" className="header">
+        <Navbar.Brand className="mr-auto">React with Typescript</Navbar.Brand>
+        <Form className="form" inline>
+          {isSignin ? (
+            <Form.Label className="username">{username}</Form.Label>
+          ) : (
+            <Button
+              variant="outline-light"
+              onClick={() => history.push('/signin')}
+            >
+              Signin
+            </Button>
+          )}
+        </Form>
+      </Navbar>
+    </>
+  );
 }
 
-export default Header;
+export default withRouter(Header);
